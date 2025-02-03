e2e135ba3e537a1169440d3f15bca1ee15c48d3b
hitomi-key <71751405+hitomi-key@users.noreply.github.com>
2025-02-04 00:32:31 +0900

    update: keymap modernization in proto_split

--- keyboards/proto_split/keymaps/default/keymap.c
+++ keyboards/proto_split/keymaps/default/keymap.c
@@ -33,47 +33,78 @@ enum custom_keycodes {
     KC_LSPC,
     RAISE,
     G_RAISE,
+    KC_RSLS,
     KC_RENT,
     ADJUST
 };
 #define KC_TCTL MT(MOD_LCTL,KC_TAB)
 
+/*
+imported keymap from current equinox
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QWERTY] = LAYOUT( //Base 
+    KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,\
+    KC_TCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,\
+    KC_LSFT, KC_BSPC, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSLS,\
+    KC_LALT, XXXXXXX, KC_LGUI,          XXXXXXX,        KC_LSPC,       XXXXXXX,      KC_RALT, XXXXXXX, ADJUST\
+  ),
+  [_LOWER] = LAYOUT( //Extra Keys
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,\
+    _______, XXXXXXX, KC_INS,  KC_DEL,  KC_BTN3, KC_F11,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, _______,\
+    _______, KC_BSPC, XXXXXXX, KC_BSPC, KC_APP,  KC_ENT,  KC_F12,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX,\
+    _______, XXXXXXX, _______,          XXXXXXX,        _______,        XXXXXXX,     _______, XXXXXXX, ADJUST\
+  ),
+  [_RAISE] = LAYOUT( // Num
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,\
+    _______, KC_4,    KC_5,    KC_6,    KC_DOT,  XXXXXXX, KC_LBRC, KC_MINS, KC_EQL,  KC_BSLS, KC_LPRN, KC_QUOT,\
+    _______, KC_GRV,  KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX, KC_RBRC, KC_SCLN, XXXXXXX, XXXXXXX, KC_RSFT,\
+    _______, XXXXXXX, _______,          XXXXXXX,         KC_ATAB,         XXXXXXX,     _______, _______, ADJUST\
+  ),
+  [_ADJUST] = LAYOUT( \
+    RESET,DF(_QWERTY),XXXXXXX, EEP_RST, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_UP,   XXXXXXX, XXXXXXX,\
+    KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_RGHT, XXXXXXX, XXXXXXX,\
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DOWN, XXXXXXX, _______,\
+    _______, XXXXXXX, _______,          XXXXXXX,        XXXXXXX,        XXXXXXX,     _______, XXXXXXX, _______\
+  ), \
+};
+*/
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(\
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,\
     KC_TCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,\
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ADJUST,\
-         KC_NO,   KC_NO,   LOWER,   KC_LGUI, KC_LSPC,       KC_RENT, KC_RALT, RAISE,   KC_NO,   KC_NO \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSLS, KC_RSFT,\
+         KC_LALT,   KC_NO,   LOWER,   KC_LGUI, KC_LSPC,       KC_LSPC, KC_RALT, RAISE,   KC_NO,   ADJUST \
 ),
     [_GAME] = LAYOUT(\
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,\
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,   KC_ENT,\
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_LGUI, ADJUST,\
-         KC_NO,    KC_NO,   LOWER,   KC_ENT,  KC_SPC,       G_RAISE, KC_RALT, RAISE,    KC_NO,   KC_NO \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_LGUI, KC_RSFT,\
+         KC_NO,    KC_NO,   LOWER,   KC_ENT,  KC_SPC,       G_RAISE, KC_RALT, RAISE,    KC_NO,   ADJUST \
 ),
   [_LOWER] = LAYOUT( \
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_DEL,\
     _______, KC_BTN1, KC_INS,  KC_DEL,  KC_BTN3,  KC_F11, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, XXXXXXX, XXXXXXX,\
     _______, KC_BTN2, KC_BSPC, KC_APP,  KC_ENT, KC_F12,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,\
-         KC_NO,   KC_NO,   LOWER,   _______, XXXXXXX,          KC_TAB, _______,RAISE,   KC_NO,   KC_NO\
+         _______, KC_NO,   LOWER,   _______, KC_TAB,           KC_TAB, _______,RAISE,   KC_NO,   ADJUST\
    ),
   [_RAISE] = LAYOUT( \
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,      KC_9,      KC_0,    KC_GRV,\
     _______, KC_4,    KC_5,    KC_6,    KC_DOT,  XXXXXXX, KC_LBRC, KC_MINS, KC_EQL,    KC_BSLS,   KC_LPRN, KC_QUOT,\
-    _______, KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX, KC_RBRC, KC_SLSH,S(KC_COMM), S(KC_DOT), KC_RPRN, _______,\
-         KC_NO,   KC_NO,   LOWER,   _______, KC_TAB,          XXXXXXX, _______, RAISE,   KC_NO,  KC_NO\
+    _______, KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX, KC_RBRC, KC_SCLN, XXXXXXX,   XXXXXXX,   KC_RPRN, _______,\
+         _______,   KC_NO,   LOWER,   _______, KC_TAB,          XXXXXXX, _______, RAISE,   KC_NO,  _______\
   ),
   [_G_RAISE] = LAYOUT( \
     KC_ESC,   KC_1,    KC_2,   KC_3,    KC_4,    KC_5,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_GRV,\
     _______, XXXXXXX,  KC_6,   KC_7,    KC_8,    KC_9,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_QUOT,\
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_0,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RPRN, _______,\
-         KC_NO,   KC_NO,   LOWER,   _______, KC_TAB,          XXXXXXX, _______, RAISE,   KC_NO,   KC_NO\
+         KC_NO,   KC_NO,   LOWER,   _______, KC_TAB,          XXXXXXX, _______, RAISE,   KC_NO,   _______\
   ),
   [_ADJUST] = LAYOUT( \
-    RESET,DF(_QWERTY),XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EEP_RST, XXXXXXX, KC_PSCR, KC_UP,   XXXXXXX, XXXXXXX,\
+    RESET,DF(_QWERTY),XXXXXXX, EEP_RST, XXXXXXX, XXXXXXX, EEP_RST, XXXXXXX, KC_PSCR, KC_UP,   XXXXXXX, XXXXXXX,\
     KC_CAPS, AU_ON,   AU_OFF,  MU_TOG,  MU_MOD, DF(_GAME),BL_STEP, BL_INC,  XXXXXXX, KC_LEFT, KC_RGHT, XXXXXXX,\
     _______, XXXXXXX, CK_RST,  CK_UP,   CK_DOWN,BL_TOGG,  BL_BRTG, BL_DEC,  XXXXXXX, KC_DOWN, XXXXXXX, _______,\
-         KC_NO,   KC_NO,   LOWER,   _______, XXXXXXX,          XXXXXXX, _______, RAISE,   KC_NO,   KC_NO\
+         _______, KC_NO,   LOWER,   _______, XXXXXXX,          XXXXXXX, _______, RAISE,   KC_NO,   _______\
   ), \
 };
 
@@ -143,6 +174,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 if(raise_pressed){
                     register_code(KC_ENT);
                     unregister_code(KC_ENT);
+                }
+                raise_pressed = 0;
+            }
+            return false;
+        case KC_RSLS:
+            if (record->event.pressed) {
+                raise_pressed = 1;
+
+                layer_on(_RAISE);
+                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_RAISE);
+                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+                
+                if(raise_pressed){
+                    register_code(KC_SLSH);
+                    unregister_code(KC_SLSH);
                 }
                 raise_pressed = 0;
             }
